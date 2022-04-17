61bb743524c1b841dcc7c0afc9133e91296f2192
Cyber-z3ter <hamzagreen1949@gmail.com>
2022-04-17 14:43:12 +0000

    initiate

--- mandatory/main.c
+++ mandatory/main.c
@@ -3,17 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: houazzan <houazzan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cyber-z3ter <cyber-z3ter@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 12:26:23 by houazzan          #+#    #+#             */
-/*   Updated: 2022/04/16 17:01:45 by houazzan         ###   ########.fr       */
+/*   Updated: 2022/04/17 14:36:06 by cyber-z3ter      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
 
-void	*philosopher(void *)
+void	*init_mutex(t_info *rules)
 {
+	int i;
+
+	i = rules->philo_number;
+	while (i-- > 0)
+	
 
 }
 
@@ -34,29 +39,40 @@ void	creat_philo(t_info rules)
 /*                     🅶🅴🆃_🅸🅽🅵🅾                    */
 /* **************************************************** */
 
-int	get_info(int ac, char **av, t_info rules)
+int	get_info(int ac, char **av, t_info *rules)
 {
-	rules.philo_number = ft_atoi(av[1]);
-	rules.time_to_die = ft_atoi(av[2]);
-	rules.time_to_eat = ft_atoi(av[3]);
-	rules.time_to_sleep = ft_atoi(av[4]);
-	if (ac == 7)
-		rules.number_of_meals = ft_atoi(av[5]);
+	rules->philo_number = ft_atoi(av[1]);
+	rules->time_to_die = ft_atoi(av[2]);
+	rules->time_to_eat = ft_atoi(av[3]);
+	rules->time_to_sleep = ft_atoi(av[4]);
+	rules->ate = 0;
+	rules->died = 0;
+	if (ac == 6)
+	{
+		rules->number_of_meals = ft_atoi(av[5]);
+		if (rules->number_of_meals == 0)
+			return (0);
+	}
 	else
-		rules.number_of_meals = 0;
-}
+		rules->number_of_meals = 0;
+	if (rules->philo_number < 2 || rules->philo_number > 250  || rules->time_to_die < 0 || \
+		rules->time_to_sleep < 0 || rules->time_to_sleep < 0)
+		return (0);
+	if (!init_mutex)
+		return (0);
+	return(1);
+} 
 
 /* **************************************************** */
-/*                        🅼🅰🅸🅽                       */
+/*                        🅼🅰🅸🅽                           */
 /* **************************************************** */
 
 int	main(int ac, char **av)
 {
 	t_info	rules;
 
-	if (ac < 6 || ac > 7)
+	if (ac != 5 || ac != 6)
 		return (ft_error ("Number of argument is wrong "));
-	if (!get_info(ac, av, rules))
+	if (!get_info(ac, av, &rules))
 		return (ft_error() && ft_clear());
-	creat_philo(rules);
 }
--- mandatory/philosopher.h
+++ mandatory/philosopher.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philosopher.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: houazzan <houazzan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cyber-z3ter <cyber-z3ter@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 12:08:33 by houazzan          #+#    #+#             */
-/*   Updated: 2022/04/16 16:06:50 by houazzan         ###   ########.fr       */
+/*   Updated: 2022/04/17 14:42:28 by cyber-z3ter      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,18 @@
 
 # include <stdio.h>
 # include <pthread.h>
+
 /* **************************************************** */
 /*                  🅳🅰🆃🅰 🆂🆃🆁🆄🅲🆃                  */
 /* **************************************************** */
+typedef struct philosopher
+{
+	int id;
+	int left_fork_id;
+	int right_fork_id;
+	pthread_t tread_id;
+	
+}	t_philosopher;
 
 typedef struct info
 {
@@ -26,7 +35,13 @@ typedef struct info
 	int	time_to_sleep;
 	int	time_to_die;
 	int	number_of_meals;
+	int ate;
+	int died;
+	pthread_mutex_t forks;
+	pthread_mutex_t meals;
+	t_philosopher philosopher;
 }	t_info;
+
 
 /* **************************************************** */
 /*             🆁🅴🅶🆄🅻🅰🆁 🅵🆄🅽🅲🆃🅸🅾🅽🆂              */
